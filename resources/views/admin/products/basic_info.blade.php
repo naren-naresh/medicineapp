53b6dd2fab142473620790f0748315365d4713f2
Ak172000 <ajithkumarkarunakaran2000@gmail.com>
2024-03-01 18:38:30 +0530

    product updated one

--- resources/views/admin/products/basic_info.blade.php
+++ resources/views/admin/products/basic_info.blade.php
@@ -13,14 +13,16 @@
             <h5 class="card-title">Add New Product</h5>
             <div aria-label="breadcrumb" class="col-6">
                 <ol class="breadcrumb step-container d-flex justify-content-between float-end">
-                    <li class="step-circle mx-1" onclick="displayStep(1)">Basic Information</li>
-                    <li class="step-circle mx-1" onclick="displayStep(2)">Additional Information</li>
-                    <li class="step-circle mx-1" onclick="displayStep(3)">Sales Information</li>
+                    <li class="step-circle-0 mx-1" onclick="displayStep(1)" style="border-bottom: 2px solid #385399;">Basic
+                        Information</li>
+                    <li class="step-circle-1 mx-1" onclick="displayStep(2)">Additional Information</li>
+                    <li class="step-circle-2 mx-1" onclick="displayStep(3)">Sales Information</li>
                 </ol>
             </div>
         </div>
         <div class="car-body px-5 py-3">
-            <form action="{{ route('product.basicInfo.post') }}" name="productForm" id="productForm" method="post">
+            <form action="{{ route('product.basicInfo.post') }}" name="productForm" id="productForm" method="post"
+                enctype="multipart/form-data">
                 @csrf
                 <!--basic info-->
                 <div class="step step-1">
@@ -35,29 +37,34 @@
                     <div class="row mt-2">
                         <div class="col-6">
                             <label for="category" class="required">Category</label>
-                            <ul class="bg-white mt-2 list-unstyled" name="parentCategory " id="parentCategory">
+                            <ul class="bg-white mt-2 list-unstyled" id="parentCategoryList">
                                 @foreach ($categories as $categoryName)
-                                    <li value="{{ $categoryName->id }}" class=" border-bottom px-2 py-1">
+                                    <li parentCatID="{{ $categoryName->id }}"
+                                        class="parent-category border-bottom px-2 py-1" role="button">
                                         {{ $categoryName->name }} <i class="fa fa-angle-right float-end"></i></li>
                                 @endforeach
                             </ul>
                         </div>
                         <div class="col-6">
                             <label for="category" class="required">Sub Category</label>
-                            <ul class="mt-2 bg-white list-unstyled px-2" name="childCategory" id="childCategory">
+                            <ul class="mt-2 bg-white list-unstyled px-2" id="childCategoryList">
                             </ul>
                         </div>
+                        <p for="category" id="category-error" class="error"></p>
+                        <input type="hidden" name="category" id="category">
                     </div>
-                    <div class="row"><p class="categoryPreview"></p></div>
+                    <div class="row">
+                        <p id='catPreview'></p>
+                    </div>
                     <div class="row mt-2">
                         <label for="description" class="mb-2">Product Description</label>
                         <div name="description" id="description" class="mt-2"></div>
                     </div>
-                    <div class="row mt-2">
+                    <div class="row mt-2" id="imgDiv">
                         <label class="mb-3">Product Image</label>
-                        <input type="file" name="image" id="image" class="mt-2 d-none"></input>
-                        <label for="image" class="mb-3" style="width: fit-content;"><span
-                                class="py-3 px-2 bg-white text-secondary">image</span></label>
+                        <label for="image" class="mb-3 picture d-flex justify-content-center align-items-center"
+                            tabIndex="0"><img src="" class="imgPreview w-100 "></img></label>
+                        <input type="file" name="image[]" id="image" class="mt-2 d-none" multiple>
                     </div>
                     <div class="row mt-2">
                         <div class="form-inline">
@@ -198,24 +205,44 @@
                         }
                     });
                 })
+
+                var categoryObject = {
+                    parentCategory: null,
+                    childCategory: null,
+                };
+                var flag = false;
                 /* passing parent id data to controller*/
-                $('#parentCategory li').click(function() {
-                    var data = $(this).val();
-                    $(this).css("background-color", "lightblue");
-                    $('.categoryPreview').append(data);
+                $('.parent-category').click(function() {
+                    let parentCatId = $(this).attr('parentCatID');
+                    $('#parentCategoryList').find('.parent-category').removeClass('active');
+                    $(this).addClass('active');
+                    categoryObject.parentCategory = {
+                        name: $(this).text()
+                    };
+                    categoryObject.childCategory = null;
+                    categoryPreview(parentCatId);
                     $.ajax({
                         data: {
-                            'parentId': data
+                            'parentId': parentCatId
                         },
                         url: "{{ route('product.basicInfo') }}",
                         type: "get",
                         success: function(data) {
-                            $('#childCategory').empty();
+                            $('#childCategoryList').empty();
                             for (item of data) {
-                                $('#childCategory').append("<li class='border-bottom py-1'>" + item.name +
+                                $('#childCategoryList').append(
+                                    "<li class='child-category border-bottom py-1' role='button'>" +
+                                    item.name +
                                     "</li>")
-                                    $('.categoryPreview').text(item.name)
                             }
+                            $('.child-category').click(function() {
+                                $('#childCategoryList').find('.child-category').removeClass('active');
+                                $(this).addClass('active');
+                                categoryObject.childCategory = {
+                                    name: $(this).text()
+                                };
+                                categoryPreview(parentCatId);
+                            });
                         },
                         error: function(data) {
                             console.log('Error:', data);
@@ -223,11 +250,26 @@
                         }
                     });
                 });
-                $('#childCategory li').click(function() {
-                    $(this).css("background-color", "lightblue");
+
+                function categoryPreview(id) {
+                    let categoryNames = "The Selected Category Was : ";
+                    if (categoryObject.parentCategory) {
+                        categoryNames += categoryObject.parentCategory.name + " ";
+                    }
+                    if (categoryObject.childCategory) {
+                        categoryNames += "-> " + categoryObject.childCategory.name;
+                    }
+                    $('#catPreview').html(categoryNames);
+                    $("#category").val(id);
+                    $('#category-error').text('');
+                }
+                $(".next-step").click(function() {
+                    if ($('#category').val() != "") {
+                        flag = true;
+                    } else {
+                        $('#category-error').text('Select your product category');
+                    }
                 });
-                $('#parentCategory li').css("cursor", "pointer");
-                $('#childCategory').css("cursor", "pointer");
                 // Product form design js
                 var currentStep = 1;
                 $('#productForm').find('.step').slice(1).hide();
@@ -235,9 +277,10 @@
                     var basic = $('#productForm');
                     basic.validate({
                         rules: {
+                            category: {
+                                required: true
+                            },
                             productName: 'required',
-                            parentCategory: 'required',
-                            childCategory: 'required',
                             manufacturer: 'required',
                             manufacturerDate: 'required',
                             expiryDate: 'required',
@@ -248,8 +291,10 @@
                             required: "This field is required",
                         },
                     });
-                    if (basic.valid() == true) {
+                    if (basic.valid() == true && flag == true) {
                         if (currentStep < 3) {
+                            $('.step-circle-' + currentStep).css('border-bottom', '2px solid #385399');
+                            $('.step-circle-' + (currentStep - 1)).css('border-bottom', 'unset');
                             $(".step-" + currentStep);
                             currentStep++;
                             setTimeout(function() {
@@ -269,6 +314,8 @@
                 // }
                 $(".prev-step").click(function() {
                     if (currentStep > 1) {
+                        $('.step-circle-' + (currentStep - 2)).css('border-bottom', '2px solid #385399');
+                        $('.step-circle-' + (currentStep - 1)).css('border-bottom', 'unset');
                         $(".step-" + currentStep);
                         currentStep--;
                         setTimeout(function() {
@@ -276,6 +323,23 @@
                             $(".step-" + currentStep).show();
                         });
                     }
+                });
+                // dynamic images upload
+                var readURL = function(input) {
+                    let imgDesign="<label for='image' class='ms-2 mb-3 picture d-flex justify-content-center align-items-center'><img src='' class='imgPreview w-100'></img></label>";
+                    if (input.files && input.files[0]) {
+                        let length=$('.picture').length;
+                        console.log(length);
+                        $('#imgDiv').after('#image').append(imgDesign);
+                        var reader = new FileReader();
+                        reader.onload = function(e) {
+                            $('.imgPreview').attr('src', e.target.result);
+                        }
+                        reader.readAsDataURL(input.files[0]);
+                    }
+                }
+                $("#image").change(function (){
+                    readURL(this);
                 });
             </script>
         @endpush
